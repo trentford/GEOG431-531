1b7dfbfa398073603f0cca9203617744272bd7d3
trentford <39271112+trentford@users.noreply.github.com>
2019-01-18 09:45:18 -0600

    Add files via upload

---
+++ SurfaceStations/compute_daily.py
@@ -0,0 +1,133 @@
+"""Compute daily summaries of ASOS/METAR data
+
+Called from RUN_12Z.sh for the previous date
+"""
+from __future__ import print_function
+import sys
+import datetime
+import warnings
+
+import numpy as np
+import pandas as pd
+from pandas.io.sql import read_sql
+import metpy.calc as mcalc
+from metpy.units import units as munits
+from pyiem.util import get_dbconn
+
+# bad values into mcalc
+warnings.simplefilter("ignore", RuntimeWarning)
+
+
+def clean(val, floor, ceiling):
+    """Make sure RH values are always sane"""
+    if val > ceiling or val < floor or pd.isna(val):
+        return None
+    if isinstance(val, munits.Quantity):
+        return float(val.magnitude)
+    return float(val)
+
+
+def do(ts):
+    """Process this date timestamp"""
+    asos = get_dbconn('asos', user='nobody')
+    iemaccess = get_dbconn('iem')
+    icursor = iemaccess.cursor()
+    df = read_sql("""
+    select station, network, iemid, drct, sknt,
+    valid at time zone tzname as localvalid,
+    tmpf, dwpf from
+    alldata d JOIN stations t on (t.id = d.station)
+    where (network ~* 'ASOS' or network = 'AWOS')
+    and valid between %s and %s and t.tzname is not null
+    and date(valid at time zone tzname) = %s
+    ORDER by valid ASC
+    """, asos, params=(ts - datetime.timedelta(days=2),
+                       ts + datetime.timedelta(days=2),
+                       ts.strftime("%Y-%m-%d")), index_col=None)
+    # derive some parameters
+    df['relh'] = mcalc.relative_humidity_from_dewpoint(
+        df['tmpf'].values * munits.degF,
+        df['dwpf'].values * munits.degF).to(munits.percent)
+    df['feel'] = mcalc.apparent_temperature(
+        df['tmpf'].values * munits.degF,
+        df['relh'].values * munits.percent,
+        df['sknt'].values * munits.knots
+    )
+    df['u'], df['v'] = mcalc.wind_components(
+        df['sknt'].values * munits.knots,
+        df['drct'].values * munits.deg
+    )
+    df['localvalid_lag'] = df.groupby('iemid')['localvalid'].shift(1)
+    df['timedelta'] = df['localvalid'] - df['localvalid_lag']
+    ndf = df[pd.isna(df['timedelta'])]
+    df.loc[ndf.index.values, 'timedelta'] = pd.to_timedelta(
+            ndf['localvalid'].dt.hour * 3600. +
+            ndf['localvalid'].dt.minute * 60., unit='s'
+    )
+    df['timedelta'] = df['timedelta'] / np.timedelta64(1, 's')
+
+    table = "summary_%s" % (ts.year,)
+    for iemid, gdf in df.groupby('iemid'):
+        if len(gdf.index) < 6:
+            # print(" Quorum not meet for %s" % (gdf.iloc[0]['station'], ))
+            continue
+        ldf = gdf.copy()
+        ldf.interpolate(inplace=True)
+        totsecs = ldf['timedelta'].sum()
+        avg_rh = clean((ldf['relh'] * ldf['timedelta']).sum() / totsecs, 1,
+                       100)
+        min_rh = clean(ldf['relh'].min(), 1, 100)
+        max_rh = clean(ldf['relh'].max(), 1, 100)
+
+        uavg = (ldf['u'] * ldf['timedelta']).sum() / totsecs
+        vavg = (ldf['v'] * ldf['timedelta']).sum() / totsecs
+        drct = clean(
+            mcalc.wind_direction(uavg * munits.knots, vavg * munits.knots),
+            0, 360)
+        avg_sknt = clean(
+            (ldf['sknt'] * ldf['timedelta']).sum() / totsecs, 0, 150  # arb
+        )
+        max_feel = clean(ldf['feel'].max(), -150, 200)
+        avg_feel = clean(
+            (ldf['feel'] * ldf['timedelta']).sum() / totsecs, -150, 200
+        )
+        min_feel = clean(ldf['feel'].min(), -150, 200)
+
+        def do_update():
+            """Inline updating"""
+            icursor.execute("""
+            UPDATE """ + table + """
+            SET avg_rh = %s, min_rh = %s, max_rh = %s,
+            avg_sknt = %s, vector_avg_drct = %s,
+            min_feel = %s, avg_feel = %s, max_feel = %s
+            WHERE
+            iemid = %s and day = %s
+            """, (avg_rh, min_rh, max_rh, avg_sknt, drct,
+                  min_feel, avg_feel, max_feel,
+                  iemid, ts))
+        do_update()
+        if icursor.rowcount == 0:
+            print(('compute_daily Adding %s for %s %s %s'
+                   ) % (table, gdf.iloc[0]['station'], gdf.iloc[0]['network'],
+                        ts))
+            icursor.execute("""
+                INSERT into """ + table + """
+                (iemid, day) values (%s, %s)
+            """, (iemid, ts))
+            do_update()
+
+    icursor.close()
+    iemaccess.commit()
+    iemaccess.close()
+
+
+def main(argv):
+    """Go Main Go"""
+    ts = datetime.date.today() - datetime.timedelta(days=1)
+    if len(argv) == 4:
+        ts = datetime.date(int(argv[1]), int(argv[2]), int(argv[3]))
+    do(ts)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ SurfaceStations/iem_scraper_example.py
@@ -0,0 +1,107 @@
+"""
+Example script that scrapes data from the IEM ASOS download service
+"""
+from __future__ import print_function
+import json
+import time
+import datetime
+# Python 2 and 3: alternative 4
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
+# Number of attempts to download data
+MAX_ATTEMPTS = 6
+# HTTPS here can be problematic for installs that don't have Lets Encrypt CA
+SERVICE = "http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
+
+
+def download_data(uri):
+    """Fetch the data from the IEM
+
+    The IEM download service has some protections in place to keep the number
+    of inbound requests in check.  This function implements an exponential
+    backoff to keep individual downloads from erroring.
+
+    Args:
+      uri (string): URL to fetch
+
+    Returns:
+      string data
+    """
+    attempt = 0
+    while attempt < MAX_ATTEMPTS:
+        try:
+            data = urlopen(uri, timeout=300).read().decode('utf-8')
+            if data is not None and not data.startswith('ERROR'):
+                return data
+        except Exception as exp:
+            print("download_data(%s) failed with %s" % (uri, exp))
+            time.sleep(5)
+        attempt += 1
+
+    print("Exhausted attempts to download, returning empty data")
+    return ""
+
+
+def get_stations_from_filelist(filename):
+    """Build a listing of stations from a simple file listing the stations.
+
+    The file should simply have one station per line.
+    """
+    stations = []
+    for line in open(filename):
+        stations.append(line.strip())
+    return stations
+
+
+def get_stations_from_networks():
+    """Build a station list by using a bunch of IEM networks."""
+    stations = []
+    states = """AK AL AR AZ CA CO CT DE FL GA HI IA ID IL IN KS KY LA MA MD ME
+     MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT
+     WA WI WV WY"""
+    # IEM quirk to have Iowa AWOS sites in its own labeled network
+    networks = ['AWOS']
+    for state in states.split():
+        networks.append("%s_ASOS" % (state,))
+
+    for network in networks:
+        # Get metadata
+        uri = ("https://mesonet.agron.iastate.edu/"
+               "geojson/network/%s.geojson") % (network,)
+        data = urlopen(uri)
+        jdict = json.load(data)
+        for site in jdict['features']:
+            stations.append(site['properties']['sid'])
+    return stations
+
+
+def main():
+    """Our main method"""
+    # timestamps in UTC to request data for
+    startts = datetime.datetime(2012, 8, 1)
+    endts = datetime.datetime(2012, 9, 1)
+
+    service = SERVICE + "data=all&tz=Etc/UTC&format=comma&latlon=yes&"
+
+    service += startts.strftime('year1=%Y&month1=%m&day1=%d&')
+    service += endts.strftime('year2=%Y&month2=%m&day2=%d&')
+
+    # Two examples of how to specify a list of stations
+    stations = get_stations_from_networks()
+    # stations = get_stations_from_filelist("mystations.txt")
+    for station in stations:
+        uri = '%s&station=%s' % (service, station)
+        print('Downloading: %s' % (station, ))
+        data = download_data(uri)
+        outfn = '%s_%s_%s.txt' % (station, startts.strftime("%Y%m%d%H%M"),
+                                  endts.strftime("%Y%m%d%H%M"))
+        out = open(outfn, 'w')
+        out.write(data)
+        out.close()
+
+
+if __name__ == '__main__':
+    main()
